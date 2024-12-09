ec54dcd096801a4c2b86f0e0889c0cf9b3434ccc
Golen <golen87@hotmail.com>
2024-12-09 23:54:46 +0100

    Clear sessions upon restart

--- communication/apps.py
+++ communication/apps.py
@@ -2,5 +2,13 @@ from django.apps import AppConfig
 
 
 class CommunicationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'communication'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "communication"
+
+    def ready(self):
+        from django.db import connection
+
+        if connection.connection and connection.connection.is_connected():
+            # Clear the sessions table on app startup
+            from django.contrib.sessions.models import Session
+            Session.objects.all().delete()
